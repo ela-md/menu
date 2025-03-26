0bcbb6361e95a4d63ea1a35f23cb021656e55432
ela.md <ela.ahmadi06@gmail.com>
2025-03-26 14:42:04 -0400

    categories added

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,19 @@
-
+import { useContext } from "react"
+import { AppContext } from "../context"
 
 function Categories () {
+    const {categories, filterItems} = useContext(AppContext);
 
     return(
-     <div>
-        <h2>Categories</h2>
-        
-     </div>
+   <div className="btn-container">
+
+  {categories.map((category) => {
+    return <button type="button" className="btn" key={category} onClick={()=> filterItems(category) }>
+        {category}
+    </button>
+  })}
+
+   </div>
     )
 }
 export default Categories
